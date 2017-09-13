14ea41d103edbe3ab9665602c503b9f07951a58c
Tom Orth <torth212@gmail.com>
2017-09-12 23:01:11 -0400

    removed unnecessary check of index

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 bot.on("message", (message) => {
-if (message.author.id !== 'YOUR ID HERE') return;
-if (message.content.indexOf('lmao') != -1) {
-    after = message.content.replace('lmao', 'lmao™')
+    if (message.author.id !== 'YOUR ID HERE') return;
+    after = message.content.replace('lmao', 'lmao™');
     message.edit(after);
-  }
 });
 bot.on("ready", () => {
     console.log("I am ready!");
